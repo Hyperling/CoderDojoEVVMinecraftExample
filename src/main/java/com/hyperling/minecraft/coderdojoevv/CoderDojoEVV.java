c317338524f44e3f49cce2647ab7fdca10b7c99e
Hyperling <hyperling2@gmail.com>
2018-12-28 11:02:49 -0600

    Added /heal

--- src/main/java/com/hyperling/minecraft/coderdojoevv/CoderDojoEVV.java
+++ src/main/java/com/hyperling/minecraft/coderdojoevv/CoderDojoEVV.java
@@ -1,22 +1,20 @@
-package com.hyperling.minecraft.coderdojotest;
+package com.hyperling.minecraft.coderdojoevv;
 
-import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.Material;
+import org.bukkit.attribute.Attributable;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerLoginEvent;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class CoderDojoTest extends JavaPlugin implements Listener {
-	public CoderDojoTest() {
-		
-	}
+public final class CoderDojoEVV extends JavaPlugin implements Listener {
     
 	@Override
 	public void onEnable() {
@@ -53,6 +51,10 @@ public final class CoderDojoTest extends JavaPlugin implements Listener {
 			inv.addItem(item);
 			item = new ItemStack(Material.DIAMOND_SPADE);
 			inv.remove(Material.DIAMOND_SPADE);
+			/* For 1.13 API
+			item = new ItemStack(Material.DIAMOND_SHOVEL);
+			inv.remove(Material.DIAMOND_SHOVEL);
+			*/
 			inv.addItem(item);
 			item = new ItemStack(Material.DIAMOND_HOE);
 			inv.remove(Material.DIAMOND_HOE);
@@ -79,6 +81,26 @@ public final class CoderDojoTest extends JavaPlugin implements Listener {
 			getServer().broadcastMessage("Inventory has been cleared for Creative mode.");
 		}
 
-		getLogger().info("Refreshed diamond items.");
+		getLogger().info("Finished onJoin().");
+	}
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		Player player = (Player) sender;
+
+		getLogger().info(player.getName() + " issued command with label " + label + ".");
+		
+		if (label.equals("heal")) {
+			double health = player.getAttribute(Attribute.GENERIC_MAX_HEALTH).getDefaultValue();
+			int food = (int) health;
+			
+			player.setHealth(health);
+			player.setFireTicks(0);
+			player.setFoodLevel(food);
+			getLogger().info(player.getName() + " has healed.");
+		}
+		
+		getLogger().info("Finished onCommand().");
+		return super.onCommand(sender, command, label, args);
 	}
 }
